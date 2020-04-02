4d23d1e21bc0262e2a3758b686e6432bf41571b6
lickerish <kacper.lakomski@gmail.com>
2020-04-02 20:19:44 +0200

    Add Bottom Navigation Bar

--- app/src/main/java/pl/lickerish/mobiletrailers/MainActivity.java
+++ app/src/main/java/pl/lickerish/mobiletrailers/MainActivity.java
@@ -1,8 +1,11 @@
 package pl.lickerish.mobiletrailers;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(menuItem -> {
+            Fragment selectedFragment = null;
+
+            switch (menuItem.getItemId()) {
+                case R.id.nav_home: {
+                    selectedFragment = new HomeFragment();
+                    break;
+                }
+                case R.id.nav_search: {
+                    selectedFragment = new SearchFragment();
+                    break;
+                }
+                case R.id.nav_about: {
+                    selectedFragment = new AboutFragment();
+                    break;
+                }
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return true;
+        });
     }
 }
